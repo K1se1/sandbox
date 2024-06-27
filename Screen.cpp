0ffa5fab4e72ce224f47f00f35fd294113ac6dcd
K1se1 <dim4k1sel@gmail.com>
2024-06-28 01:59:08 +0700

    Отрисовка квадрата на openGL заняла чуть больше времени, чем я думал... В следующих коммитах функция отрисовки квадратов будет вынесена в отдельный метод, попытаюсь отрисовать массив таких квадратов

--- Screen.cpp
+++ Screen.cpp
@@ -16,6 +16,12 @@ namespace Graphics
     }
     void Screen::Display()
     {
+        GLfloat vertices[] = {-0.5f, -0.5f, 0.0f,
+                            -0.5f, 0.5f, 0.0f,
+                            0.5f, 0.5f, 0.0f,
+                            0.5f, -0.5f, 0.0f};
+        GLuint indices[] = {0, 1, 2,
+                            0, 2, 3 };
         _window = glfwCreateWindow(_height, _width, "SandBox", NULL, NULL);
         if(_window == NULL)
         {
@@ -23,17 +29,58 @@ namespace Graphics
             glfwTerminate();
         }
         glfwMakeContextCurrent(_window);
+        gladLoadGL();
         glViewport(0, 0, 800, 800);
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(_window); 
+        //Обрабатываем шейдеры
+        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); // вершинныЙ шейдер
+        glShaderSource(vertexShader, 1, &_vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        
+        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); // фрагментный шейдер
+        glShaderSource(fragmentShader, 1, &_fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+   
+        GLuint shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        GLuint VAO, VBO, EBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0 );
+        glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
          while(!glfwWindowShouldClose(_window))
         {
+            glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glfwSwapBuffers(_window); 
             glfwPollEvents();
         }
     }
     Screen::~Screen()
     {
+
         glfwDestroyWindow(_window);
         glfwTerminate();
     }
--- Screen.hpp
+++ Screen.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include "C:/msys64/mingw64/lib/glad/include/glad/glad.h"
 #include<GLFW/glfw3.h>
 #include<string> 
+#include<cmath>
 #include "RGBColor.hpp"
 namespace Graphics
 {
@@ -11,6 +13,19 @@ namespace Graphics
         int _height, _width;
         std::string _name;
         RGBColor _color;
+        const char* _vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+        const char* _fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+        "}\n\0";
         public:
         Screen();
         Screen(int height, int width, std::string name, RGBColor color);
